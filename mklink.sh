03059b411fdda09e82be16765a540a3383a2e2bd
Kenji Morimoto <k.morimoto@ebisuinc.jp>
2019-08-13 18:15:06 +0900

    VIM以外の設定を一時削除

--- mklink.sh
+++ mklink.sh
@@ -4,17 +4,17 @@ cd `dirname $0`
 SCR_DIR=`pwd`
 
 # zsh関係
-ln -sf $SCR_DIR/.zshrc $HOME/.zshrc
-ln -sf $SCR_DIR/.zshenv $HOME/.zshenv
+#ln -sf $SCR_DIR/.zshrc $HOME/.zshrc
+#ln -sf $SCR_DIR/.zshenv $HOME/.zshenv
 
 # Vim関係
 if [ ! -d $HOME/.vim ] ; then
   mkdir $HOME/.vim
 fi
-if [ ! -d $HOME/.vim/bundle ] ; then
-  mkdir $HOME/.vim/bundle
-  git clone https://github.com/Shougo/dein.vim $HOME/.vim/bundle/dein.vim
-fi
+#if [ ! -d $HOME/.vim/bundle ] ; then
+#  mkdir $HOME/.vim/bundle
+#  git clone https://github.com/Shougo/dein.vim $HOME/.vim/bundle/dein.vim
+#fi
 ln -sf $SCR_DIR/.vimrc $HOME/.vimrc
 ln -sf $SCR_DIR/.gvimrc $HOME/.gvimrc
 ln -sf $SCR_DIR/.vim/morimoto $HOME/.vim/morimoto
@@ -26,5 +26,5 @@ ln -sf $SCR_DIR/.gitignore_global $HOME/.gitignore_global
 ln -sf $SCR_DIR/.gitconfig.wsl $HOME/.gitconfig
 
 # tmux
-ln -sf $SCR_DIR/.tmux.conf $HOME/.tmux.conf
+#ln -sf $SCR_DIR/.tmux.conf $HOME/.tmux.conf
 
